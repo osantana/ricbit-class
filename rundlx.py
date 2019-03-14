c83e164419e72c90e6e80192ab61841d3731a7a7
Osvaldo Santana Neto <osantana@osantana.me>
2019-03-14 00:13:08 -0300

    Add frontend for dlx

---
+++ rundlx.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import argparse
+import subprocess
+
+
+path = os.path.abspath(os.path.dirname(__file__))
+dlx = os.path.join(path, 'dlx')
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      description = "Run a variant of the dlx binary")
+  binary = parser.add_mutually_exclusive_group()
+  binary.add_argument("--sharp", help="Use the sharp heuristic", action="store_true")
+  binary.add_argument("--skip", help="Use the skip heuristic", action="store_true")
+  binary.add_argument("--dlx1", help="Use dlx1 (original)", action="store_true")
+  binary.add_argument("--dlx2", help="Use dlx2 (variant with colors)", action="store_true")
+  binary.add_argument("--dlx3", help="Use dlx3 (variant with multiplicity)", action="store_true")
+  binary.add_argument("--dlx5", help="Use dlx5 (variant with cost)", action="store_true")
+  speed = parser.add_mutually_exclusive_group()
+  speed.add_argument("--fast", help="Output progress fast", action="store_true")
+  speed.add_argument("--faster", help="Output progress faster", action="store_true")
+  parser.add_argument("--pre", help="Run the preprocessor", nargs=1,
+      choices=["pipe", "stop"])
+  parser.add_argument("--verbose", help="Verbose output", action="store_true")
+  parser.add_argument("--stop", help="Stop at the nth solution", nargs="?",
+      default=None, type=int, metavar="n")
+  parser.add_argument("template", help="File template")
+  args = parser.parse_args()
+  if args.sharp:
+    executable = "dlx2.sharp"
+  elif args.skip:
+    executable = "dlx2.skip"
+  elif args.dlx1:
+    executable = "dlx1"
+  elif args.dlx3:
+    executable = "dlx3"
+  elif args.dlx5:
+    executable = "dlx5"
+  else:
+    executable = "dlx2"
+  verbose = "v391" if args.verbose else ""
+  if args.fast:
+    ticks = "d100000000"
+  elif args.faster:
+    ticks = "d10000000"
+  else:
+    ticks = "d1000000000"
+  stop = "t%d" % args.stop if args.stop else ""
+  if args.pre and args.pre[0] == "stop":
+    commandline = f'(time {dlx}/dlx-pre < {args.template}.dlx) > {args.template}.pre.dlx '
+  else:
+    if args.pre and args.pre[0] == "pipe":
+      commandpipe = f'{dlx}/dlx-pre < %s.dlx | {dlx}/%s m1 %s %s %s' % (args.template, executable, ticks, stop, verbose)
+    else:
+      commandpipe = (f'{dlx}/%s m1 %s %s %s < %s.dlx ' %
+          (executable, ticks, stop, verbose, args.template))
+    commandline = ('(time (%s)) 2>&1 | tee %s.out.txt | ggrep -v -P "^(\s(?!after)|\d+:|Level|L\d+)"' % (commandpipe, args.template))
+  subprocess.call(commandline, shell=True)
+
+main()
+
+
